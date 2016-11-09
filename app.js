93501999a4dec362710d2af3b0a015f576209d84
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-09 23:56:10 +0530

    Adding script on Wed Nov  9 23:56:10 IST 2016 :blush:

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const { app, BrowserWindow } = electron
 
 let mainWindow = null
 
-app.on('ready', () => {
+const createWindow = () => {
+
   console.log('The application is ready.')
 
   mainWindow = new BrowserWindow({width: 1280, height: 1024})
 
   mainWindow.loadURL('file://'+ __dirname + '/src/index.html')
 
+	mainWindow.webContents.openDevTools()
+  
   mainWindow.on('closed', function() {
-    mainWindow = null
-  })
-})
+  	mainWindow = null
+	})
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin')
+    app.quit()
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open.
+  if (win === null)
+    createWindow()
+});
